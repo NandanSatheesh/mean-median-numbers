a75eef6e798c7dcdf719329080032fb2ac310025
NandanSatheesh <nandan.199776@gmail.com>
2020-10-31 04:02:37 +0530

    fix api endpoint string

--- src/main/java/com/example/numbers/constants/NumberApplicationConstants.java
+++ src/main/java/com/example/numbers/constants/NumberApplicationConstants.java
@@ -4,7 +4,7 @@ public class NumberApplicationConstants {
     public static final String API = "/api";
     public static final String VERSION_ONE = "/v1.0";
     public static final String NUMBER = "/number";
-    public static final String MEAN_AND_MEDIAN = "/mean_median";
+    public static final String MEAN_AND_MEDIAN = "/mean_and_median";
 
     public static final String SUCCESS = "Success";
 }
